8e952f4da0cb685c8abf1b5ef3160b003ea90202
attakei <attakei@gmail.com>
2016-08-20 02:07:57 +0900

    Implement facts

--- idcf/idcf_dns_zone_facts.py
+++ idcf/idcf_dns_zone_facts.py
@@ -38,13 +38,59 @@ zones:
 '''
 
 
+import time
+import hmac
+from base64 import b64encode
+from httplib import HTTPSConnection
+
+
+class IDCFClient(object):
+    """IDCF-cloud DNS service API client
+    """
+    HOSTNAME = 'dns.idcfcloud.com'
+    BASE_PATH = '/api/v1'
+
+    def __init__(self, api_key, secret_key):
+        self.api_key = api_key
+        self.secret_key = secret_key
+
+    def generate_signature(self, method, path, expires):
+        base_string = '\n'.join([
+            method, path, self.api_key, str(expires), ''
+        ])
+        hash = hmac.new(self.secret_key, base_string, AVAILABLE_HASH_ALGORITHMS['sha256'])
+        return b64encode(hash.digest())
+
+    def request(self, method, path, data=None, expires=60):
+        expires = int(time.time()) + expires
+        path = self.BASE_PATH + path
+        signature = self.generate_signature(method, path, expires)
+        headers = {
+            'X-IDCF-APIKEY': self.api_key,
+            'X-IDCF-Expires': expires,
+            'X-IDCF-Signature': signature,
+        }
+        conn = HTTPSConnection(self.HOSTNAME)
+        conn.request(method, path, headers=headers)
+        resp = conn.getresponse()
+        return json.loads(resp.read())
+
+
 def main():
     # Define module settings
-    module = AnsibleModule({
-
-    })
+    argument_spec = {
+        'idcf_api_key': {
+            'required': True
+        },
+        'idcf_secret_key': {
+            'required': True
+        },
+    }
+    module = AnsibleModule(argument_spec=argument_spec)
     # Proceed
-    module.exit_json()
+    client = IDCFClient(module.params['idcf_api_key'], module.params['idcf_secret_key'])
+    zones = client.request('GET', '/zones')
+    module.exit_json(changed=False, zones=zones)
 
 
 from ansible.module_utils.docker_common import *
