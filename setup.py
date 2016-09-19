013d8220558c804071fe0ddb76f5fa0253c90324
Kazuya Takei <attakei@gmail.com>
2016-09-20 01:18:34 +0900

    Refactor setup.py

--- setup.py
+++ setup.py
@@ -2,27 +2,43 @@
 from setuptools import setup
 import sys
 import os
+import codecs
+
 
 here = os.path.dirname(__file__)
+sys.path.append(here)
+
+
+def readme(filename="README.rst"):
+    fullpath = os.path.join(here, filename)
+    readme_ = "" 
+    with codecs.open(fullpath, encoding='utf-8') as fp:
+        readme_ = fp.read()
+    return readme_
+
 
 install_requires = [
     "ansible",
 ]
 
 
+import idcf
+
+
 setup(
     name="ansible-modules-idcf",
-    version="0.0.1",
+    version=idcf.__version__,
 
-    description="Ansible modules for IDCF-cloud",
-    long_description=open(os.path.join(here, "README.rst")).read(),
+    description=idcf.__doc__,
+    long_description=readme(),
     license="GPLv3",
     url="https://github.com/attakei/ansible-modules-idcf",
 
     author="kAZUYA tAKEI",
 
     classifiers=[
-        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
